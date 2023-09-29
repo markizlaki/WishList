6afccb75c16068842a8d6fe5233205f4743777b3
Max Pog <pogorelovmaxim@gmail.com>
2023-09-29 12:48:31 +0300

    add burger and avtorization

--- scripts/renderNavigation.js
+++ scripts/renderNavigation.js
@@ -1,14 +1,15 @@
+import { API_URL, JWT_TOKEN_KEY } from "./const.js";
+import { createBurgerMenu } from "./createBurgerMenu.js";
 import { createElement } from "./helper.js";
+import { auth, router } from "./index.js";
+import { renderModal } from "./renderModal.js";
 
 const nav = document.querySelector(".nav");
 
-// const burger = createBurgerMenu(nav);
+createBurgerMenu(nav, 'nav_active', '.nav__btn');
 
 export const renderNavigation = () => {
     nav.textContent = '';
-    
-    // <button class="nav__btn btn">Зарегистрироваться</button>
-    // <button class="nav__btn btn">Войти</button>
 
     const buttonSignUp = createElement("button", {
         className: "nav__btn btn",
@@ -16,7 +17,41 @@ export const renderNavigation = () => {
     })
 
     buttonSignUp.addEventListener("click", () => {
+        renderModal({
+            title: "регистрация",
+            description: "Введите Ваши данные для регистрации на сервисе WishList",
+            btnSubmit: "Зарегистрироваться",
+            async submitHandler(event) {
+                const formData = new FormData(event.target);
+                const credentials = {
+                    login: formData.get('login'),
+                    password: formData.get('password'),
+                };
 
+                try {
+                    const response = await fetch(`${API_URL}/register`, {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(credentials)
+                    });
+
+                    if (response.ok) {
+                        const data = await response.json();
+                        localStorage.setItem(JWT_TOKEN_KEY, data.token);
+                        auth.login = data.login;
+                        router.setRoute(`/user/${data.login}`);
+
+                        return true;
+                    } else {
+                        const { message = 'Unknown error' } = await response.json();
+                        console.log(message);
+                        throw new Error(message);
+                    }
+                } catch (error) {
+                    alert(error.message);
+                }
+            }
+        })
     })
 
     const buttonLogin = createElement("button", {
